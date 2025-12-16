f1a145225f62b733453fb03d7fe204f4b039f3cd
a2 <a2namoff@gmail.com>
2025-12-15 20:28:11 -0500

    Last

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-    public static void main(String[] args)
-    throws ThreadDeath {
+    public static void main(String[] args) {
         System.out.println("Hello World");
-        Thread.stop();
+        System.out.println("www");
     }
 }
